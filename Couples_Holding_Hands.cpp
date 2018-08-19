0e4e04180e58d0d9ff035dd8effd7ae539fe72bb
Chia-Hao Chang <lineagech@gmail.com>
2018-08-19 13:06:11 +0800

    Update Couples_Holding_Hands.cpp

--- Couples_Holding_Hands.cpp
+++ Couples_Holding_Hands.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
-    /**
-     * @param row: the couples' initial seating
-     * @return: the minimum number of swaps
-     */
     int minSwapsCouples(vector<int> &row) {
-        // Write your code here
         int n = row.size();
         if( n % 2 != 0 ) return 0;
         int ans = 0;
@@ -19,17 +14,11 @@ public:
             if( couple_2 != row[i+1] ) //swap
             {   
                 int tmp_row_value = row[i+1];
-                swap_vec(row, i+1, pos[couple_2]);//swap(row[i+1], row[pos[couple_2]]);
-                swap_vec(pos, tmp_row_value, couple_2);//swap(pos[row[i+1]], pos[couple_2]);
+                swap(row[i+1], row[pos[couple_2]]);
+                swap(pos[tmp_row_value], pos[couple_2]);
                 ans++;
             }
         }
         return ans;
-    }
-    void swap_vec(vector<int>& arr, int pos1, int pos2)
-    {
-        int tmp = arr[pos2];
-        arr[pos2] = arr[pos1];
-        arr[pos1] = tmp;
     }
 };
