5a62b23c221e6c224fae20871e2792d0d37910a2
Chia-Hao Chang <lineagech@gmail.com>
2018-10-03 16:07:51 +0800

    Update Insert-Delete-GetRandom-O(1).cpp
    Fix bugs

--- Insert-Delete-GetRandom-O(1).cpp
+++ Insert-Delete-GetRandom-O(1).cpp
@@ -1,31 +1,24 @@
 class RandomizedSet {
 public:
+    /** Initialize your data structure here. */
     RandomizedSet() {
-        // do intialization if necessary
+        
     }
-
-    /*
-     * @param val: a value to the set
-     * @return: true if the set did not already contain the specified element or false
-     */
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        // write your code here
         if(hash.find(val)!=hash.end()){
             return false;
         }
         
         nums.push_back(val);
-        hash[val]=nums.size();
+        hash[val]=nums.size()-1;
         
         return true;
     }
-
-    /*
-     * @param val: a value from the set
-     * @return: true if the set contained the specified element or false
-     */
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        // write your code here
         if(hash.find(val)==hash.end()){
             return false;
         }
@@ -38,15 +31,20 @@ public:
         
         return true;
     }
-
-    /*
-     * @return: Get a random element from the set
-     */
+    
+    /** Get a random element from the set. */
     int getRandom() {
-        // write your code here
-        return rand()%nums.size();
+        return nums[rand()%nums.size()];
     }
+    
     vector<int> nums;
     unordered_map<int,int> hash;
-    
 };
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * bool param_1 = obj.insert(val);
+ * bool param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
