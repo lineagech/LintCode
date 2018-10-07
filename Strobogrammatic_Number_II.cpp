41f6e9cc1ead2a643225a98f1f2aad698db8809e
Chia-Hao Chang <lineagech@gmail.com>
2018-10-07 13:35:10 +0800

    Iterative Solution

--- Strobogrammatic_Number_II.cpp
+++ Strobogrammatic_Number_II.cpp
@@ -4,55 +4,33 @@ public:
      * @param n: the length of strobogrammatic number
      * @return: All strobogrammatic numbers
      */
-    int step = 0;
+    string pattern[5] = {"1","6","8","9","0"};
     vector<string> findStrobogrammatic(int n) {
-        // write your code here
-        // 0<->0
-        // 1<->1 
-        // 6<->9
-        // 8<->8 
-        // 9<->6
-        char pattern[5] = {'1', '6', '8', '9', '0'};
-        vector<string> ans;
-        if( n == 0 ) {
-            ans.push_back("");
+        vector<string> q;
+        if( n == 0 ) return {""};
+        if( n % 2 == 0 ) {
+            if( n != 2 ) q.push_back("00");
+            q.push_back("11");
+            q.push_back("69");
+            q.push_back("88");
+            q.push_back("96");
+        } else {
+            q.push_back("0");
+            q.push_back("1");
+            q.push_back("8");
         }
-        if( n == 1 ) {
-            ans.push_back("0");
-            ans.push_back("1");
-            ans.push_back("8");
-        }
-        else if( n == 2 )
-        {
-            ans.push_back("11");
-            ans.push_back("69");
-            ans.push_back("88");
-            ans.push_back("96");
-        }
-        else if( n > 2 ){
-            step++;
-            vector<string> tmp = findStrobogrammatic(n-2);
-            step--;
-            if( n-2 == 2 ) tmp.insert(tmp.begin(), "00");
-            for( auto p : tmp )
-            {   
-                string next;
-                for( int i=0; i<4; i++ )
-                {
-                    next = p;
-                    next.insert(next.begin(), 1, pattern[i]);
-                    next.insert(next.end(), 1, pattern[(4-i)%4]);
-                    ans.push_back(next);
-                }
-                if( step != 0 )
-                {
-                    next = p;
-                    next.insert(next.begin(), 1, pattern[4]);
-                    next.insert(next.end(), 1, pattern[4]);
-                    ans.push_back(next);
-                }
+        for( int i = 4-n%2; i <= n; i+=2 )
+        {   
+            vector<string> tmp;
+            for( auto p : q ) {
+                if( i != n ) tmp.push_back(pattern[4]+p+pattern[4]);
+                tmp.push_back(pattern[0]+p+pattern[0]);
+                tmp.push_back(pattern[1]+p+pattern[3]);
+                tmp.push_back(pattern[2]+p+pattern[2]);
+                tmp.push_back(pattern[3]+p+pattern[1]);
             }
+            q = tmp;
         }
-        return ans;
+        return q;
     }
 };
